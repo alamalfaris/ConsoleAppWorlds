72bf8471e9a077f07a8647e4e3937461f62f17e8
Alam Alfaris <61654794+alamalfaris@users.noreply.github.com>
2023-07-27 12:16:34 +0700

    feat: create log4net utility and console log4net

---
+++ ConsoleLog4Net/Program.cs
@@ -0,0 +1,38 @@
+﻿using log4net.Config;
+using log4net;
+using System.Reflection;
+using Log4Net.LogUtility;
+
+namespace ConsoleLog4Net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LoggingInitialize();
+            var logs = new Logger();
+
+            logs.Info("Starting the application");
+
+            try
+            {
+                logs.Debug($"Starting method {MethodBase.GetCurrentMethod()?.DeclaringType}");
+                throw new Exception("Sample Error inside the try catch block code");
+            }
+            catch (Exception ex)
+            {
+                logs.Error(ex.Message, ex.InnerException);
+            }
+
+            logs.Debug("Waiting for user input");
+            Console.ReadLine();
+            logs.Info("Ending application");
+        }
+
+        static void LoggingInitialize()
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+        }
+    }
+}
