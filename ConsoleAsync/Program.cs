58d0970a5cf07c178425b6766bde72ed25f85c32
alam.alfaris <alam.alfaris@axa-mandiri.co.id>
2023-07-27 22:05:07 +0700

    feat: create async console app basic

---
+++ ConsoleAsync/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleAsync
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Method1();
+			Method2();
+			Console.ReadKey();
+		}
+
+		public static async Task Method1()
+		{
+			await Task.Run(() =>
+			{
+				for (int i = 0; i < 100; i++)
+				{
+					Console.WriteLine(" Method 1");
+				}
+			});
+		}
+
+		//public static void Method1()
+		//{
+		//	for (int i = 0; i < 100; i++)
+		//	{
+		//		Console.WriteLine(" Method 1");
+		//	}
+		//}
+
+		public static void Method2()
+		{
+			for (int i = 0; i < 25; i++)
+			{
+				Console.WriteLine(" Method 2");
+			}
+		}
+	}
+}
