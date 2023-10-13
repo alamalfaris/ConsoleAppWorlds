b01550cf61853ac0a5fb70c822269a710b90512d
alam.alfaris <alam.alfaris@axa-mandiri.co.id>
2023-10-13 15:12:33 +0700

    feat: create ConsoleRegex and ConsoleReflection

---
+++ ConsoleRegex/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRegex
+{
+	public interface IRepository
+	{
+		Task<(int, string, string)> InsertDataProduct1(string param1, string param2);
+	}
+}
---
+++ ConsoleRegex/Repository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRegex
+{
+	public class Repository : IRepository
+	{
+		public async Task<(int, string, string)> InsertDataProduct1(string param1, string param2)
+		{
+			return await Task.FromResult((1, "yes", "yes"));
+		} 
+	}
+}
