a90d48fa5dcbf81c9500cd8d48b6039d22d381aa
alam.alfaris <alam.alfaris@axa-mandiri.co.id>
2023-10-13 20:11:04 +0700

    feat: create console regex for regex trial

---
+++ ConsoleRegex/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRegex
+{
+	public class Customer
+	{
+		public string CustomerName { get; set; } = string.Empty;
+		public string Gender { get; set; } = string.Empty;
+		public string IdentificationNo { get; set; } = string.Empty;
+		public string Email { get; set; } = string.Empty;
+		public string BirthDate { get; set; } = string.Empty;
+	}
+}
---
+++ ConsoleRegex/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Reflection;
+
+namespace ConsoleRegex
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+
+			Customer customer = new()
+			{
+				CustomerName = "Alam",
+				Gender = "k",
+				IdentificationNo = "1234567890",
+				Email = "alam123@gmail.com",
+				BirthDate = "01/06/1997"
+			};
+
+			Validation1(customer); // not good
+			Validation2(customer); // good
+		}
+
+		static void Validation1(Customer customer)
+		{
+			if (Validations.IsNotAOrB(customer.Gender, "L", "P")
+				&& Validations.IsDatePatternValid(customer.BirthDate)
+				&& Validations.IsEmailPatternValid(customer.Email)
+				&& Validations.IsOnlyNumeric(customer.IdentificationNo)
+				&& Validations.IsOnlyAlphabeth(customer.CustomerName)
+				)
+			{
+				Console.WriteLine("Kena validasi1");
+			}
+			else
+			{
+				Console.WriteLine("Ga kena validasi1");
+			}
+		}
+
+		static void Validation2(Customer customer)
+		{
+			(bool, string) validationResult;
+
+			validationResult = Validations.CheckValueNotAOrB(customer.Gender, "L", "P");
+			if (!validationResult.Item1)
+			{
+				Console.WriteLine($"Value: {validationResult.Item2} invalid");
+			}
+
+			validationResult = Validations.OnlyAlphabeth(customer.CustomerName);
+			if (!validationResult.Item1)
+			{
+				Console.WriteLine($"Value: {validationResult.Item2} invalid");
+			}
+
+			validationResult = Validations.OnlyNumeric(customer.IdentificationNo);
+			if (!validationResult.Item1)
+			{
+				Console.WriteLine($"Value: {validationResult.Item2} invalid");
+			}
+
+			validationResult = Validations.CheckDatePattern(customer.BirthDate);
+			if (!validationResult.Item1)
+			{
+				Console.WriteLine($"Value: {validationResult.Item2} invalid");
+			}
+
+			validationResult = Validations.CheckEmailPattern(customer.Email);
+			if (!validationResult.Item1)
+			{
+				Console.WriteLine($"Value: {validationResult.Item2} invalid");
+			}
+		}
+	}
+}
